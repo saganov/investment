9f9e118cd74e75f3081099e856091061a1c1405c
Petro Saganov <petro.saganov@oracle.com>
2018-02-28 19:37:46 +0200

    [feat] Adds implementation of function Loan::invest()

--- src/Loan.php
+++ src/Loan.php
@@ -17,17 +17,31 @@ class Loan
 {
     private $start;
     private $end;
+    private $investments = [];
     public function __construct(DateTime $start, DateTime $end){
         $this->start = $start;
         $this->end = $end;
     }
-    public function invest(Investor $investor, Tranch $tranch, $sum){
-
+    public function invest($investment){
+        $this->investments[] = $investment;
     }
     public function calculateInterest(DateTime $date){
         if ($this->start <= $date && $date <= $this->end){
-            $report = "'Investor 1' earns 28.06 pounds\n'Investor 3' earns 21.29 pounds";
-            return $report;
+            $investors = [];
+            foreach ($this->investments as $investment){
+                $investor = $investment['investor'];
+                $earned = $investment['sum'] * $investment['interest'] * ($date->diff($investment['date'], true)->format('%a') + 1) / 31;
+                if (key_exists((string)$investor, $investors)) {
+                    $investors[(string)$investor] += $earned;
+                } else {
+                    $investors[(string)$investor] = $earned;
+                }
+            }
+            $report = [];
+            foreach ($investors as $name => $earns){
+                $report[] = sprintf("'%s' earns %01.2f pounds", $name, round($earns, 2));
+            }
+            return implode("\n", $report);
         } else {
             throw new \Exception("Unavailable date '{$date->format('d/m/Y')}' for the loan. Valid range is '{$this->start->format('d/m/Y')}' - '{$this->end->format('d/m/Y')}'");
         }
--- tests/LoanTest.php
+++ tests/LoanTest.php
@@ -10,9 +10,11 @@ class LoanTest extends TestCase
 
     public function testInterestCalculate(){
         $loan = new Loan(new DateTime('2015-10-01'), new DateTime('2015-11-15'));
+        $loan->invest(['investor' => 'Investor 1', 'sum' => 1000, 'interest' => 0.03, 'date' => new DateTime('2015-10-03')]);
+        $loan->invest(['investor' => 'Investor 3', 'sum' => 500, 'interest' => 0.06, 'date' => new DateTime('2015-10-10')]);
         $this->assertEquals(
             "'Investor 1' earns 28.06 pounds\n'Investor 3' earns 21.29 pounds",
-            $loan->calculateInterest(new DateTime('2015-11-01')));
+            $loan->calculateInterest(new DateTime('2015-10-31')));
     }
     public function testInterestCalculateWithInvalidDate(){
         $loan = new Loan(new DateTime('2015-10-01'), new DateTime('2015-11-15'));
